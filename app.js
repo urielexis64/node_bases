199330b4122d34de076988ecfdce4e6b8608f87a
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-25 16:04:19 -0700

    getting parameters as args through console

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const {createMultiplyFile} = require("./helpers/multiply");
+const argv = require("./config/yargs");
+
 console.clear();
 
-const base = 5;
+let base = argv.b;
+let list = argv.l;
+let lim = argv.m;
 
-createMultiplyFile(base)
+console.log(argv);
+
+createMultiplyFile(base, list, lim)
 	.then((fileName) => console.log(fileName, "created"))
 	.catch((err) => console.log(err));
