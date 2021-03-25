0798a85340a5a5f56c992bf6d8c00735bde1dd9c
Uriel Aispuro <urielalexis64@gmail.com>
2021-03-25 16:03:49 -0700

    using colors library

--- helpers/multiply.js
+++ helpers/multiply.js
@@ -1,17 +1,25 @@
 const fs = require("fs");
+require("colors");
 
-const createMultiplyFile = async (base = 1) => {
+const createMultiplyFile = async (base = 1, list = false, lim = 10) => {
 	try {
-		let salida = "";
+		let salida = "",
+			salidaConsola = "";
 
-		for (let index = 1; index <= 10; index++) {
+		for (let index = 1; index <= lim; index++) {
+			salidaConsola += `${base} ${"x".red} ${index} = ${base * index}\n`;
 			salida += `${base} x ${index} = ${base * index}\n`;
 		}
 
-		console.log(salida);
+		if (list) {
+			console.log("=================");
+			console.log(`  Tabla del: ${base}  `.rainbow.bold);
+			console.log("=================");
+			console.log(salidaConsola);
+		}
 
-		fs.writeFileSync(`table-${base}.txt`, salida);
-		return `table-${base}.txt`;
+		fs.writeFileSync(`./output/table-${base}-${lim}.txt`, salida);
+		return `table-${base}-${lim}.txt`.green;
 	} catch (err) {
 		throw err;
 	}
